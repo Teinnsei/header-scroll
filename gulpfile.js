3812f6c2f54bbfc2b318e968b3dca477aa2e47fb
teinnsei <teinnsei@icloud.com>
2016-04-13 02:38:18 +0300

    Created the example page
    Fix gulp structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp     = require('gulp');
 var uglify   = require('gulp-uglify');
 var rename   = require('gulp-rename');
-gulp.task('clean', function(done) {
-    rimraf('dist', done);
-});
 
 gulp.task('default', function() {
     return gulp.src('src/js/header-scroll.js')
