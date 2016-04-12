378301d5f900cfeb4d04081841ec36e4ac8b2550
teinnsei <teinnsei@icloud.com>
2016-04-13 02:38:03 +0300

    Created the dist files

---
+++ dist/js/header-scroll.js
@@ -0,0 +1,58 @@
+/*!
+ * header-scroll v1.0.0
+ * by Alexander Galagutskiy
+ *
+ * More info:
+ * https://github.com/Teinnsei/header-scroll
+ *
+ * Copyright 2016 Alexander Galagutskiy
+ * Released under the MIT license
+ * https://github.com/Teinnsei/header-scroll/blob/master/LICENSE
+ */
+
+(function($) {
+    $.fn.headerScroll = function(options) {
+        var opts = $.extend({}, $.fn.headerScroll.defaults, options);
+        var position = $(opts.section).eq(opts.sectionShow).offset().top;
+        this.css({"top" : position, "display": opts.display, "position": "fixed"});
+        if(opts.enableFixedHeader) {
+            $(document).on('scroll', function() {
+                var position = $(opts.section).eq(opts.sectionShow).offset().top;
+                var scroll = $(window).scrollTop();
+                if(scroll > position) {
+                    $(this).css({"top": 0});
+                }
+                else {
+                    if(scroll > 0) position -= scroll;
+                    $(this).css({"top": position});
+                }
+            }.bind(this));
+        }
+        if(opts.enableActiveLink) {
+            $(document).on('scroll', function() {
+                var headerS = this;
+                $(opts.section).each(function() {
+                    var scroll = $(window).scrollTop();
+                    var topSection = $(this).offset().top;
+                    var index = $(this).index();
+                    if(topSection <= (scroll)) {
+                        headerS
+                            .find(opts.linkTag)
+                            .removeClass(opts.activeLinkClass)
+                            .eq(index - 1)
+                            .addClass(opts.activeLinkClass);
+                    }
+                });
+            }.bind(this));
+        }
+    };
+    $.fn.headerScroll.defaults = {
+        enableFixedHeader: true,
+        section: "section",
+        sectionShow: 1,
+        display: "block",
+        enableActiveLink: true,
+        activeLinkClass: 'active',
+        linkTag: 'li'
+    };
+})(jQuery);
