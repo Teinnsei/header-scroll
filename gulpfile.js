e97cf24d3af9a6a360c79fd5d3eb94552bbb28fa
teinnsei <teinnsei@icloud.com>
2016-04-13 01:42:04 +0300

    Created tasks and added dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-var gulp = require('gulp');
+var gulp     = require('gulp');
+var uglify   = require('gulp-uglify');
+var rename   = require('gulp-rename');
+gulp.task('clean', function(done) {
+    rimraf('dist', done);
+});
+
+gulp.task('default', function() {
+    return gulp.src('src/js/header-scroll.js')
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(rename('header-scroll.min.js'))
+        .pipe(uglify({preserveComments:"license"}))
+        .pipe(gulp.dest('dist/js/'));
+});
+
+
